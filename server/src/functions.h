87084292fc638094bd9dae7084c8b28102ca3889
Nejern <5qub6pfp@duck.com>
2023-03-29 18:36:04 +0300

    Added doxygen

--- server/src/functions.h
+++ server/src/functions.h
@@ -1,9 +1,9 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-#include <QString>
 #include <QByteArray>
 #include <QMap>
+#include <QString>
 #include <QStringList>
 
 QByteArray log_in(QString username, QString password);
@@ -14,10 +14,9 @@ QByteArray give_a_role(QString username, QString new_role);
 
 QByteArray change_role(QString username, QString new_role);
 
-QByteArray change_pass(QString old_pass, QString new_pass1,  QString new_pass2);
+QByteArray change_pass(QString old_pass, QString new_pass1, QString new_pass2);
 
 QByteArray add_user(QString username, QString new_role);
-
 
 QByteArray show_pass(QString account);
 
