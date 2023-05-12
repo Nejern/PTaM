29b1b865a8938ff86792765553b12afe49f094f2
Evgenii Safronov <emsafronov2004@gmail.com>
2023-05-13 02:07:53 +0300

    Added getGrades functional to server

--- server/include/functions.h
+++ server/include/functions.h
@@ -8,7 +8,7 @@
  * сервера.
  */
 class ServerFunctions {
-public:
+ public:
   /**
    * @brief Разбирает входящее сообщение и вызывает соответствующую функцию.
    * @param message Входящее сообщение для разбора.
@@ -16,7 +16,7 @@ public:
    */
   static QByteArray parse(const QString &message);
 
-private:
+ private:
   /**
    * @brief Выбирает команду из JSON и вызывает соответствующую функцию.
    * @param json JSON документ с командой и данными.
@@ -37,4 +37,5 @@ private:
    * @return Результат выполнения команды аутентификации.
    */
   static QByteArray loginUser(const QJsonObject &json);
+  static QByteArray getGrades();
 };
