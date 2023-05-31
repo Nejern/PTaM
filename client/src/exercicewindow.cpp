39d9fbbdcbccca6ac8b1229a1384849c4d573348
Nejern <5qub6pfp@duck.com>
2023-05-31 17:09:39 +0300

    Completed task 1

--- client/src/exercicewindow.cpp
+++ client/src/exercicewindow.cpp
@@ -4,7 +4,7 @@
 
 #include "ui_exercicewindow.h"
 
-ExerciceWindow::ExerciceWindow(QWidget* parent, int excerciseNumber)
+ExerciceWindow::ExerciceWindow(QWidget *parent, int excerciseNumber)
     : QMainWindow(parent), ui(new Ui::ExerciceWindow) {
   ui->setupUi(this);
   connect(ui->answerButton, SIGNAL(clicked()), this,
@@ -22,69 +22,110 @@ void ExerciceWindow::setExcercise() {
   // excerciseRawData - данные для задачи для сервера (сюда то что от вашего
   // генератора засунуть надо будет)
   switch (excerciseId) {
-    case 1:
-      ui->excerciseText->setText("1");
-      break;
 
-    case 2:
-      ui->excerciseText->setText("2");
-      break;
-    case 3: {
-      // Установка текста задачи
-      ui->excerciseText->setText("Составьте код прюффера для графа:");
+  case 1: {
+    // Установка текста задачи
+    ui->excerciseText->setText("Постройте минимальный каркас графа.");
 
-      // Генерация данных для задачи
-      int numNodes = QRandomGenerator::global()->bounded(4, 10);
-      QString generatedData = QString::number(numNodes) + " ";
+    // Генерация данных для задачи
+    int numNodes = QRandomGenerator::global()->bounded(4, 10);
+    QString generatedData = QString::number(numNodes) + " ";
 
-      QList<int> nodes;
-      for (int i = 0; i < numNodes; ++i) {
-        nodes.append(i);
+    QList<QPair<int, int>> edges;
+    for (int i = 1; i < numNodes; ++i) {
+      int node1 = QRandomGenerator::global()->bounded(i);
+      int node2 = i;
+      edges.append(qMakePair(node1, node2));
+      generatedData +=
+          QString::number(node1) + " " + QString::number(node2) + " ";
+    }
+
+    // Сохранение данных для задачи чтобы потом кинуть в сервер
+    excerciseRawData = generatedData.trimmed();
+    qDebug() << excerciseRawData;
+
+    // Форматирование данных для ui
+    QString formattedExcerciseData;
+    QStringList tokens = generatedData.split(' ');
+    if (!tokens.isEmpty()) {
+      int numNodes = tokens.takeFirst().toInt();
+      formattedExcerciseData += QString::number(numNodes) + " ";
+
+      while (tokens.size() >= 2) {
+        int node1 = tokens.takeFirst().toInt();
+        int node2 = tokens.takeFirst().toInt();
+        formattedExcerciseData += "(" + QString::number(node1) + ", " +
+                                  QString::number(node2) + "), ";
       }
 
-      std::random_device rd;
-      std::mt19937 g(rd());
-      std::shuffle(nodes.begin(), nodes.end(), g);
+      if (!formattedExcerciseData.isEmpty()) {
+        formattedExcerciseData.chop(2);
+      }
+    }
 
-      for (int i = 1; i < numNodes; ++i) {
-        int parent = nodes.at(QRandomGenerator::global()->bounded(i));
-        int child = nodes.at(i);
-        generatedData +=
-            QString::number(parent) + " " + QString::number(child) + " ";
+    // Установка данных для задачи в ui
+    ui->excerciseData->setText(formattedExcerciseData.trimmed());
+  } break;
+
+  case 2:
+    ui->excerciseText->setText("2");
+    break;
+  case 3: {
+    // Установка текста задачи
+    ui->excerciseText->setText("Составьте код прюффера для графа:");
+
+    // Генерация данных для задачи
+    int numNodes = QRandomGenerator::global()->bounded(4, 10);
+    QString generatedData = QString::number(numNodes) + " ";
+
+    QList<int> nodes;
+    for (int i = 0; i < numNodes; ++i) {
+      nodes.append(i);
+    }
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(nodes.begin(), nodes.end(), g);
+
+    for (int i = 1; i < numNodes; ++i) {
+      int parent = nodes.at(QRandomGenerator::global()->bounded(i));
+      int child = nodes.at(i);
+      generatedData +=
+          QString::number(parent) + " " + QString::number(child) + " ";
+    }
+
+    // Сохранение данных для задачи чтобы потом кинуть в сервер
+    excerciseRawData = generatedData.trimmed();
+    qDebug() << excerciseRawData;
+
+    // Форматирование данных для для ui
+    QString formattedExcerciseData;
+    QStringList tokens = generatedData.split(' ');
+    if (!tokens.isEmpty()) {
+      int numNodes = tokens.takeFirst().toInt();
+      formattedExcerciseData += QString::number(numNodes) + " ";
+
+      while (tokens.size() >= 2) {
+        int node1 = tokens.takeFirst().toInt();
+        int node2 = tokens.takeFirst().toInt();
+        formattedExcerciseData += "(" + QString::number(node1) + ", " +
+                                  QString::number(node2) + "), ";
       }
 
-      // Сохранение данных для задачи чтобы потом кинуть в сервер
-      excerciseRawData = generatedData.trimmed();
-      qDebug() << excerciseRawData;
-
-      // Форматирование данных для для ui
-      QString formattedExcerciseData;
-      QStringList tokens = generatedData.split(' ');
-      if (!tokens.isEmpty()) {
-        int numNodes = tokens.takeFirst().toInt();
-        formattedExcerciseData += QString::number(numNodes) + " ";
-
-        while (tokens.size() >= 2) {
-          int node1 = tokens.takeFirst().toInt();
-          int node2 = tokens.takeFirst().toInt();
-          formattedExcerciseData += "(" + QString::number(node1) + ", " +
-                                    QString::number(node2) + "), ";
-        }
-
-        if (!formattedExcerciseData.isEmpty()) {
-          formattedExcerciseData.chop(2);
-        }
+      if (!formattedExcerciseData.isEmpty()) {
+        formattedExcerciseData.chop(2);
       }
+    }
 
-      // установка данных для задачи в ui
-      ui->excerciseData->setText(formattedExcerciseData.trimmed());
-    } break;
-    case 4:
-      ui->excerciseText->setText("4");
-      break;
-    case 5:
-      ui->excerciseText->setText("5");
-      break;
+    // установка данных для задачи в ui
+    ui->excerciseData->setText(formattedExcerciseData.trimmed());
+  } break;
+  case 4:
+    ui->excerciseText->setText("4");
+    break;
+  case 5:
+    ui->excerciseText->setText("5");
+    break;
   }
 }
 
--- server/src/excercises.cpp
+++ server/src/excercises.cpp
@@ -5,11 +5,66 @@
 #include <QMap>
 #include <QStack>
 
+// построить минимальный каркас графа
 bool Excercises::task1(QString answer, QString exercise_data) {
-  if (answer == exercise_data) {
-    return true;
+  QStringList inputList = exercise_data.split(' ', Qt::SkipEmptyParts);
+  int numNodes = inputList.takeFirst().toInt();
+
+  QList<QPair<int, int>> edges;
+  while (!inputList.isEmpty()) {
+    int node1 = inputList.takeFirst().toInt();
+    int node2 = inputList.takeFirst().toInt();
+    edges.append(qMakePair(node1, node2));
   }
-  return false;
+
+  // Сортируем ребра по весу
+  std::sort(edges.begin(), edges.end(),
+            [](const QPair<int, int> &edge1, const QPair<int, int> &edge2) {
+              return edge1.first < edge2.first;
+            });
+
+  QList<QPair<int, int>> minimumSpanningTree;
+  int numEdges = 0;
+
+  QMap<int, int> vertexSets;
+  for (int i = 0; i < numNodes; ++i) {
+    vertexSets[i] = i;
+  }
+
+  for (const auto &edge : edges) {
+    int root1 = vertexSets[edge.first];
+    int root2 = vertexSets[edge.second];
+
+    if (root1 != root2) {
+      // Ребро не создаст цикл, добавляем его в каркас
+      minimumSpanningTree.append(edge);
+      numEdges++;
+
+      if (numEdges == numNodes - 1) {
+        // Достигли необходимого количества ребер
+        break;
+      }
+
+      // Объединяем наборы вершин
+      for (auto &vertex : vertexSets) {
+        if (vertex == root2) {
+          vertex = root1;
+        }
+      }
+    }
+  }
+
+  // Формируем строку с ребрами каркаса
+  QString minimumSpanningTreeStr;
+  for (const auto &edge : minimumSpanningTree) {
+    minimumSpanningTreeStr.append(
+        QString("(%1, %2) ").arg(edge.first).arg(edge.second));
+  }
+  minimumSpanningTreeStr = minimumSpanningTreeStr.trimmed();
+  qDebug() << minimumSpanningTreeStr;
+
+  // Сравниваем результат алгоритма с ответом пользователя
+  return minimumSpanningTreeStr == answer;
 }
 
 bool Excercises::task2(QString answer, QString exercise_data) {
@@ -35,7 +90,7 @@ bool Excercises::task3(QString answer, QString exercise_data) {
     degrees[i] = 0;
   }
 
-  for (const auto& edge : edges) {
+  for (const auto &edge : edges) {
     degrees[edge.first]++;
     degrees[edge.second]++;
   }
@@ -44,7 +99,7 @@ bool Excercises::task3(QString answer, QString exercise_data) {
 
   for (int i = 0; i < numNodes - 2; ++i) {
     int leafNode = -1;
-    for (const auto& node : degrees.keys()) {
+    for (const auto &node : degrees.keys()) {
       if (degrees[node] == 1) {
         leafNode = node;
         break;
@@ -53,12 +108,12 @@ bool Excercises::task3(QString answer, QString exercise_data) {
 
     if (leafNode == -1) {
       qWarning() << "Invalid input. Unable to build Pruefer code.";
-      return false;  // тут может быть баг
+      return false; // тут может быть баг
     }
 
     degrees.remove(leafNode);
 
-    for (const auto& edge : edges) {
+    for (const auto &edge : edges) {
       if (edge.first == leafNode || edge.second == leafNode) {
         degrees[edge.first]--;
         degrees[edge.second]--;
