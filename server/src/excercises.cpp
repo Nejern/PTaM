fb7f68959f09526a72daf4e0e9bf734e109638ce
Aluhug <94222690+Aluhug@users.noreply.github.com>
2023-06-02 14:25:32 +0300

    add task 5

--- server/src/excercises.cpp
+++ server/src/excercises.cpp
@@ -3,7 +3,90 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QMap>
+#include <QQueue>
 #include <QStack>
+#include <QString>
+#include <QVector>
+#include <iostream>
+#include <limits>
+
+// класс для 5 задания
+class NetworkGraph {
+ public:
+  NetworkGraph(int vertices) {
+    this->vertices = vertices;
+    adjacencyMatrix =
+        QVector<QVector<int>>(vertices, QVector<int>(vertices, 0));
+  }
+
+  // Добавление ребра в граф
+  void addEdge(int from, int to, int capacity) {
+    if (from >= 0 && from < vertices && to >= 0 && to < vertices)
+      adjacencyMatrix[from][to] = capacity;
+    else
+      qDebug() << "Ошибка: некорректные индексы вершин";
+  }
+
+  // Поиск максимального потока в сети с помощью алгоритма Форда-Фалкерсона
+  int findMaxFlow(int source, int sink) {
+    // Создание остаточной сети
+    QVector<QVector<int>> residualGraph(adjacencyMatrix);
+    QVector<int> parent(vertices);
+
+    int maxFlow = 0;
+
+    while (bfs(source, sink, parent, residualGraph)) {
+      int pathFlow = std::numeric_limits<int>::max();
+
+      // Нахождение минимальной пропускной способности ребер на пути
+      for (int v = sink; v != source; v = parent[v]) {
+        int u = parent[v];
+        pathFlow = qMin(pathFlow, residualGraph[u][v]);
+      }
+
+      // Обновление остаточной сети
+      for (int v = sink; v != source; v = parent[v]) {
+        int u = parent[v];
+        residualGraph[u][v] -= pathFlow;
+        residualGraph[v][u] += pathFlow;
+      }
+
+      maxFlow += pathFlow;
+    }
+
+    return maxFlow;
+  }
+
+ private:
+  int vertices;
+  QVector<QVector<int>> adjacencyMatrix;
+
+  // Поиск пути в остаточной сети с помощью алгоритма поиска в ширину (BFS)
+  bool bfs(int source, int sink, QVector<int>& parent,
+           const QVector<QVector<int>>& residualGraph) {
+    QVector<bool> visited(vertices, false);
+    QQueue<int> queue;
+
+    visited[source] = true;
+    queue.enqueue(source);
+    parent[source] = -1;
+
+    while (!queue.isEmpty()) {
+      int u = queue.front();
+      queue.dequeue();
+
+      for (int v = 0; v < vertices; ++v) {
+        if (!visited[v] && residualGraph[u][v] > 0) {
+          visited[v] = true;
+          queue.enqueue(v);
+          parent[v] = u;
+        }
+      }
+    }
+
+    return visited[sink];
+  }
+};
 
 bool Excercises::task1(QString answer, QString exercise_data) {
   if (answer == exercise_data) {
@@ -81,8 +164,23 @@ bool Excercises::task4(QString answer, QString exercise_data) {
 }
 
 bool Excercises::task5(QString answer, QString exercise_data) {
-  if (answer == exercise_data) {
-    return true;
+  QStringList tokens = exercise_data.split(' ');
+  int numNodes = tokens.takeFirst().toInt();
+
+  NetworkGraph graph(numNodes);
+
+  int from, to, capacity;
+  while (!tokens.isEmpty()) {
+    from = tokens.takeFirst().toInt();
+    to = tokens.takeFirst().toInt();
+    capacity = tokens.takeFirst().toInt();
+    graph.addEdge(from, to, capacity);
   }
-  return false;
+
+  int source = 0;
+  int sink = numNodes - 1;
+  int maxFlow = graph.findMaxFlow(source, sink);
+  qDebug() << maxFlow;
+
+  return QString::number(maxFlow) == answer;
 }
